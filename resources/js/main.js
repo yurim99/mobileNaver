6b798e29cb6148e95ed98189e917ee2519ee0043
yurim <me_28@naver.com>
2025-03-07 16:43:48 +0900

    scss 분리

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
+
     const feedPopularBtns = document.querySelectorAll('.feed-popular__btn');
     feedPopularBtns.forEach(feedPopularBtn=> {
         feedPopularBtn.addEventListener('click', function(feedPopularBtn) {
             const nextEl = this.nextElementSibling;
-
+            
             this.classList.toggle('active');
             nextEl.classList.toggle('active');
             
         })
     }) 
-
+    
+    document.addEventListener("click", function(event) {
+        const feedPopularBtns = document.querySelector('.feed-popular__btn');
+        const feedPopularList = document.querySelector('.feed-popular__list');
+    
+        if (feedPopularBtns && !feedPopularBtns.contains(event.target)) {
+            feedPopularList.classList.remove("active");
+        }
+    });
 });
