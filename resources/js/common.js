08863c6ca61dca97f1739c3e9ad9cb5189d43c99
yurim <me_28@naver.com>
2025-03-10 14:25:26 +0900

    검색창 하단 영역 js완료

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,14 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    //header
     const mainSearchInput = document.querySelector('.main-search__input');
+    const headerSearchInput = document.querySelector('.header-search__input');
     const headerSearchStage = document.querySelector('.header-search-stage');
     const headerBackBtn = document.querySelector('.header-search__btn.back');
-
+    const searchClose = document.querySelector('.search-control-btn.close');
+    
     function showSearchStage() {
         headerSearchStage.classList.add('active');
+        headerSearchInput.focus();
     }
     function hideSearchStage() {
         headerSearchStage.classList.remove('active');
     }
     mainSearchInput.addEventListener('click', showSearchStage)
     headerBackBtn.addEventListener('click', hideSearchStage)
+    searchClose.addEventListener('click', hideSearchStage);
+    
+    //search
+    const searchAutoSave = document.getElementById('searchAutoSave');
+    
+    searchAutoSave.addEventListener('click', function() {
+        const activeAutoSave = searchAutoSave.classList.contains('on')
+        if(activeAutoSave) {
+            const result = confirm('최근검색어 저장 기능을 사용 중지하시겠습니까?');
+            if(result) {
+                searchAutoSave.classList.remove('on')
+                searchAutoSave.classList.add('off')
+            }else {
+            }
+        } else {
+            searchAutoSave.classList.remove('off')
+            searchAutoSave.classList.add('on')
+        }
+    })
 })
