d0775201ce9bad7ec49046ad480b8a5010b2608d
yurim <me_28@naver.com>
2025-03-18 14:37:46 +0900

    날씨 정보 추가하기

---
+++ resources/js/weather.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const apiKey = '4a25235891e03dd674f2b7ba12cbf13a';
+    const cities = [
+        { english: 'Seoul', korean: '서울' },
+        { english: 'Busan', korean: '부산' },
+        { english: 'Incheon', korean: '인천' },
+        { english: 'Daegu', korean: '대구' },
+        { english: 'Gwangju', korean: '광주' },
+        { english: 'Daejeon', korean: '대전' },
+        { english: 'Ulsan', korean: '울산' },
+        { english: 'Jeju', korean: '제주' },
+        { english: 'Suwon', korean: '수원' },
+        { english: 'Goyang', korean: '고양' },
+        { english: 'Seongnam', korean: '성남' },
+        { english: 'Jeonju', korean: '전주' },
+        { english: 'Changwon', korean: '창원' },
+        { english: 'Pohang', korean: '포항' },
+        { english: 'Cheongju', korean: '청주' },
+        { english: 'Andong', korean: '안동' },
+        { english: 'Ansan', korean: '안산' },
+        { english: 'Cheonan', korean: '천안' },
+        { english: 'Wonju', korean: '원주' },
+        { english: 'Iksan', korean: '익산' },
+        { english: 'Pyeongtaek', korean: '평택' },
+        { english: 'Gangneung', korean: '강릉' },
+        { english: 'Jeongeup', korean: '정읍' },
+        { english: 'Mokpo', korean: '목포' },
+        { english: 'Yeosu', korean: '여수' },
+        { english: 'Sokcho', korean: '속초' },
+        { english: 'Naju', korean: '나주' },
+        { english: 'Bucheon', korean: '부천' }
+        // { english: 'Gimpo', korean: '김포' },
+        // { english: 'Gwangmyeong', korean: '광명' },
+      ];
+
+    cities.forEach(city => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.english}&appid=${apiKey}&units=metric&lang=kr`;
+        const weatherStage = document.querySelector('.weather-stage');
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if(data.cod === 200) {
+                    const temperature = data.main.temp.toFixed(1);
+                    const weather = data.weather[0].description;
+                    
+                    const feedWeatherWrap = document.createElement('div');
+                    feedWeatherWrap.classList.add('feed-weather');
+                    feedWeatherWrap.innerHTML = `
+                        <div class="feed-weather__icon"></div>
+                        <span class="sr-only">${weather}</span>
+                        <p class="feed-weather__data">${temperature}°</p>
+                        <p class="feed-weather__area">${city.korean}</p>
+                    `
+                    weatherStage.appendChild(feedWeatherWrap);
+
+                    console.log(`${city.english} (${city.korean}) - 기온: ${temperature}°C, 날씨: ${weather}`);
+                }
+            })
+            .catch(error => {
+                console.error(`${city.english} (${city.korean})의 날씨 데이터를 가져오는 데 실패했습니다:`, error);
+            });
+
+            
+    })
+
+    
+})
